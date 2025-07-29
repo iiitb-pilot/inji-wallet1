e26aca2754fbbeaa1e5483eebfb21d77db96680d
Suraj-Murgude <suraj.murgude@infystrat.com>
2025-07-29 17:17:37 +0530

    Update VCItemField.tsx
    Signed-off-by: Suraj-Murgude <suraj.murgude@infystrat.com>

--- components/VC/common/VCItemField.tsx
+++ components/VC/common/VCItemField.tsx
@@ -1,10 +1,15 @@
-import {Dimensions, View} from 'react-native';
-import {Column, Row, Text} from '../../ui';
-import {CustomTooltip} from '../../ui/ToolTip';
-import {Theme} from '../../ui/styleUtils';
 import React from 'react';
-import {SvgImage} from '../../ui/svg';
-import {useTranslation} from 'react-i18next';
+import {View} from 'react-native';
+import {Column, Row, Text} from '../../ui';
+import {Theme} from '../../ui/styleUtils';
+
+interface VCItemFieldProps {
+  fieldName: string;
+  fieldValue: string;
+  testID: string;
+  fieldNameColor?: string;
+  fieldValueColor?: string;
+}
 
 export const VCItemFieldName = ({
   fieldName,
@@ -15,70 +20,17 @@ export const VCItemFieldName = ({
   testID: string;
   fieldNameColor?: string;
 }) => {
-  const {t} = useTranslation('ViewVcModal');
   return (
     <Row>
-      {fieldName && (
-        <Text
-          testID={`${testID}Title`}
-          color={textColor}
-          style={Theme.Styles.fieldItemTitle}>
-          {fieldName}
-        </Text>
-      )}
-
-      {fieldName == t('VcDetails:status') && (
-        <CustomTooltip
-          testID="statusToolTip"
-          width={Dimensions.get('screen').width * 0.8}
-          height={Dimensions.get('screen').height * 0.28}
-          triggerComponent={SvgImage.info()}
-          triggerComponentStyles={{marginLeft: 2, marginTop: 2}}
-          toolTipContent={
-            <Column align="flex-start">
-              <View style={{marginBottom: 20}}>
-                <Text weight="semibold">
-                  {t('statusToolTipContent.valid.title')}
-                </Text>
-                <Text
-                  weight="regular"
-                  style={[
-                    Theme.Styles.tooltipContentDescription,
-                    {marginTop: 3},
-                  ]}>
-                  {t('statusToolTipContent.valid.description')}
-                </Text>
-              </View>
-              <View style={{marginBottom: 20}}>
-                <Text weight="semibold">
-                  {t('statusToolTipContent.pending.title')}
-                </Text>
-                <Text
-                  weight="regular"
-                  style={[
-                    Theme.Styles.tooltipContentDescription,
-                    {marginTop: 3},
-                  ]}>
-                  {t('statusToolTipContent.pending.description')}
-                </Text>
-              </View>
-              <View>
-                <Text weight="semibold">
-                  {t('statusToolTipContent.expired.title')}
-                </Text>
-                <Text
-                  weight="regular"
-                  style={[
-                    Theme.Styles.tooltipContentDescription,
-                    {marginTop: 3},
-                  ]}>
-                  {t('statusToolTipContent.expired.description')}
-                </Text>
-              </View>
-            </Column>
-          }
-        />
-      )}
+      <Text
+        testID={`${testID}Title`}
+        color={textColor}
+        style={[
+          Theme.Styles.fieldItemTitle,
+          {fontWeight: 'bold'}, // Bold label
+        ]}>
+        {fieldName}
+      </Text>
     </Row>
   );
 };
@@ -93,30 +45,34 @@ export const VCItemFieldValue = ({
   fieldValueColor?: string;
 }) => {
   return (
-    <>
-      <Text
-        testID={`${testID}Value`}
-        color={textColor}
-        style={Theme.Styles.fieldItemValue}>
-        {fieldValue}
-      </Text>
-    </>
+    <Text
+      testID={`${testID}Value`}
+      color={textColor}
+      style={Theme.Styles.fieldItemValue}>
+      {fieldValue}
+    </Text>
   );
 };
 
-export const VCItemField: React.FC<VCItemFieldProps> = props => {
+export const VCItemField: React.FC<VCItemFieldProps> = ({
+  fieldName,
+  fieldValue,
+  testID,
+  fieldNameColor,
+  fieldValueColor,
+}) => {
   return (
-    <Column>
-      <VCItemFieldName {...props} />
-      <VCItemFieldValue {...props} />
+    <Column margin="8 0">
+      <VCItemFieldName
+        fieldName={fieldName}
+        testID={testID}
+        fieldNameColor={fieldNameColor}
+      />
+      <VCItemFieldValue
+        fieldValue={fieldValue}
+        testID={testID}
+        fieldValueColor={fieldValueColor}
+      />
     </Column>
   );
 };
-
-interface VCItemFieldProps {
-  fieldName: string;
-  fieldValue: string;
-  testID: string;
-  fieldNameColor?: string;
-  fieldValueColor?: string;
-}
