f748b3f27685373a7b33244241ac3f820754e46a
Suraj-Murgude <suraj.murgude@infystrat.com>
2025-07-29 17:13:40 +0530

    Update ReceivedVcsTab.tsx
    Signed-off-by: Suraj-Murgude <suraj.murgude@infystrat.com>

--- screens/Home/ReceivedVcsTab.tsx
+++ screens/Home/ReceivedVcsTab.tsx
@@ -29,7 +29,7 @@ export const ReceivedVcsTab: React.FC<HomeScreenTabProps> = props => {
             key={vcMetadata.getVcKey()}
             vcMetadata={vcMetadata}
             margin="0 2 8 2"
-            onPress={controller.VIEW_VC}
+            onPress={() => props.onCardPress(vcMetadata)} 
             flow={VCItemContainerFlowType.VC_SHARE}
           />
         ))}
